d25720a7acec42164aa0f9fe7881ffa651ebef32
danielejsantos <js_dani@hotmail.com>
2021-03-26 15:20:01 -0300

    Create global types

---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+export type Planets = {
+  climate: string;
+  created: string;
+  diameter: string;
+  edited: string;
+  films: string[];
+  gravity: string;
+  name: string;
+  orbital_period: string;
+  population: string;
+  rotation_period: string;
+  surface_water: string;
+  terrain: string;
+};
