15d3a64582efe60f6338c2acaed5b7dc3a95714c
danielejsantos <js_dani@hotmail.com>
2021-03-26 15:24:20 -0300

    Add Context Provider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import Home from './pages/Home';
+import Home from "./pages/Home";
 
-import GlobalStyles from './styles/GlobalStyles';
+import GlobalStyles from "./styles/GlobalStyles";
 
-function App() {
-  return (
-    <>
+import PlanetsProvider from "./context";
+
+const App: React.FC = () => (
+  <>
+    <PlanetsProvider>
       <Home />
+    </PlanetsProvider>
 
-      <GlobalStyles />
-    </>
-  );
-}
+    <GlobalStyles />
+  </>
+);
 
 export default App;
